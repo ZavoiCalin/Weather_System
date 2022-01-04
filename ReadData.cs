ba5912c9ce4c16d4a41a448fdfb95bfd16e154e8
ZavoiCalin <62338985+ZavoiCalin@users.noreply.github.com>
2022-01-04 18:49:20 +0200

    Update ReadData.cs

--- ReadData.cs
+++ ReadData.cs
@@ -1,1 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class WeatherData 
+{
+    public string name;
+
+    public ParticleSystem particleSystem;
+
+    //[HideInInspector]
+    public ParticleSystem.EmissionModule emission;
+
+    public bool useAudio;
+    public AudioClip weatherAudio;
+    public float audioFadeInTimer, lightIntensity, lightDimTimer, fogChangeSpeed;
+
+    public Transform windzone;
+
+    public Color fogColor, currentForColor;
+    
+    
+}
 
